155b21c9025ba91b0c41be2d89c1dc4e90aa0535
Brian Njogu <briannjogu85@gmail.com>
2025-04-04 09:04:36 +0300

    created __init__.py

--- school_mgmt/lib/models/__init__.py
+++ school_mgmt/lib/models/__init__.py
@@ -1,1 +1,7 @@
-# Initializes the models subpackage
+# lib/models/__init__.py
+
+from .student import Student
+from .classroom import Classroom
+from .enrollment import Enrollment
+
+__all__ = ["Student", "Classroom", "Enrollment"]
